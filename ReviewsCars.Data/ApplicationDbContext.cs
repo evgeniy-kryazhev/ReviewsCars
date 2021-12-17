311951e1aa4810a1e456f5ba78044564a1056bf9
Кряжев Евгений <kryazhev_evgeniy@vk.com>
2021-12-17 04:42:45 +0300

    init class lib Data

---
+++ ReviewsCars.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ReviewsCars.Entities;
+
+namespace ReviewsCars.Data;
+
+public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public DbSet<PostCar> PostCars { get; set; } = null!;
+    
+    private const string SqlConnectionString = "Data Source=app.db";
+
+    public ApplicationDbContext()
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(SqlConnectionString);
+    }
+}
