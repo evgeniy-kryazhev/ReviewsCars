2963402d94a46335024671ecbced4610addbf004
Кряжев Евгений <kryazhev_evgeniy@vk.com>
2021-12-17 04:42:05 +0300

    Init class lib Entities

---
+++ ReviewsCars.Entities/Commentary.cs
@@ -0,0 +1,8 @@
+﻿namespace ReviewsCars.Entities;
+
+public abstract class Commentary
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
---
+++ ReviewsCars.Entities/PostCar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace ReviewsCars.Entities;
+
+public class PostCar
+{
+    public Guid Id { get; set; }
+    public ApplicationUser? User { get; set; }
+    public string Name { get; set; } = null!;
+    public string Text { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+    public DateTime CreateAt { get; set; }
+}
