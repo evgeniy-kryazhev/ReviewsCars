d0f4df0dd5c3fea091512f6b28b0dab19cabd559
Кряжев Евгений <kryazhev_evgeniy@vk.com>
2021-12-17 14:22:39 +0300

    add tail wind

---
+++ ReviewsCars.Web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './Views/Shared/*.cshtml',
+    './Views/Account/*.cshtml',
+    './Views/CarReview/*.cshtml',
+    './Views/Home/*.cshtml',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
